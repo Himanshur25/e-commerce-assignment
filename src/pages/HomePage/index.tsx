20a257a577b1f5e1b0d2d676435159677d892dac
Himanshu Rathi <himanshurathi@Himanshus-MacBook-Pro.local>
2024-06-09 00:17:59 +0530

    Feat: Remove unwanted code and updated folder structure

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div className="flex text-base"> This is the Home Page</div>;
+};
+
+export default HomePage;
---
+++ src/pages/Product/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductPage = () => {
+  return <div>This is the product page</div>;
+};
+
+export default ProductPage;
